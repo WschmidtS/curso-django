293b6526c6fd7705bbae56d0ed18f053fa3642ae
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-06-08 19:59:00 -0300

    Corrigido o arquivo settings.py de acordo com a PEP8
    close #23

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '02o)wth&rtwzth3-_%&mb&*t)o-z6w7=d!wq$$lb)%l6@iaf(b'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
