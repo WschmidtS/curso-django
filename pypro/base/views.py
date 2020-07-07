3708a9cb55437bf696d09c82e43c99ad53dcd044
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-06 22:30:27 -0300

    Instalado e configurado o monitoramento de erro com o Sentry.
    close #99

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django by WschmidtS</body></html>', content_type='text/html')
+    raise ValueError()
+    return HttpResponse('<html><body>Olá Django by WschmidtS</body></html>')
