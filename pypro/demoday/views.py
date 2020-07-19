fdbd1c6c7b1cd34bb86416bb5b2f32be980dbbfb
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-19 11:02:19 -0300

    Configurado o template de vídeos para gerar várias páginas distintas utilizando o Slug.

--- pypro/demoday/views.py
+++ pypro/demoday/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(resquest, slug):
-    video = {'titulo': '6º Demo Day - Polo Digital de Mogi das Cruzes', 'vimeo_id': 439637023}
+    videos = {
+        '6demoday': {'titulo': '6º Demo Day - Polo Digital de Mogi das Cruzes', 'vimeo_id': 439637023},
+        'apresentacao_sebrae': {'titulo': 'Apresentação TecAcademy - Tarefa Sebrae', 'vimeo_id': 0000},
+    }
+    video = videos[slug]
     return render(resquest, 'demoday/video.html', context={'video': video})
