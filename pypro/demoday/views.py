de5974e13e3c70851312217970017e31b88077e6
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-25 20:10:12 -0300

    Criado um modelo para os vídeos. Falta conectar com o banco de dados se será feito na próxima aula.
    close #155

--- pypro/demoday/views.py
+++ pypro/demoday/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.vimeo_id = vimeo_id
-        self.titulo = titulo
-        self.slug = slug
-
-    def get_absolute_url(self):
-        return reverse('demoday:video', args=(self.slug,))
-
+from pypro.demoday.models import Video
 
 videos = [
-    Video('6demoday', '6º Demo Day - Polo Digital de Mogi das Cruzes', 439637023),
-    Video('apresentacao_sebrae', 'Apresentação TecAcademy - Tarefa Sebrae', 439733389),
+    Video(slug='6demoday', titulo='6º Demo Day - Polo Digital de Mogi das Cruzes', vimeo_id='439637023'),
+    Video(slug='apresentacao_sebrae', titulo='Apresentação TecAcademy - Tarefa Sebrae', vimeo_id='439733389'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
