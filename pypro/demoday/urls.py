759995c68409f8d7ac588477fe3519dd57bece17
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-25 14:16:29 -0300

    Criado lista com todos os vídeos na página de demoday.
    close #142

--- pypro/demoday/urls.py
+++ pypro/demoday/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from pypro.demoday.views import video
+from pypro.demoday.views import video, indice
 
 app_name = 'demoday'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
