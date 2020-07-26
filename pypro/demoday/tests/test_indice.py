f8c4769d275026b88bdfefcd419a8ade9c450749
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-26 19:34:55 -0300

    Alterado a página de indices para fazer acesso ao banco de dados, ordenando a lista de vídeos pela data de publicação. Alterado o teste de indices para usar a biblioteca Mommy
    close #160

--- pypro/demoday/tests/test_indice.py
+++ pypro/demoday/tests/test_indice.py
@@ -1,11 +1,18 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
+from pypro.demoday.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
+def videos(db):
+    return mommy.make(Video, 3)
+
+
+@pytest.fixture
+def resp(client, videos):
     return client.get(reverse('demoday:indice'))
 
 
@@ -13,24 +20,12 @@ def test_status_code(resp):
     assert resp.status_code == 200
 
 
-@pytest.mark.parametrize(
-    'titulo',
-    [
-        '6º Demo Day - Polo Digital de Mogi das Cruzes',
-        'Apresentação TecAcademy - Tarefa Sebrae'
-    ]
-)
-def test_titulo_video(resp, titulo):
-    assert_contains(resp, titulo)
+def test_titulo_video(resp, videos):
+    for video in videos:
+        assert_contains(resp, video.titulo)
 
 
-@pytest.mark.parametrize(
-    'slug',
-    [
-        '6demoday',
-        'apresentacao_sebrae'
-    ]
-)
-def test_link_video(resp, slug):
-    video_link = reverse('demoday:video', args=(slug,))
-    assert_contains(resp, f'href="{video_link}"')
+def test_link_video(resp, videos):
+    for video in videos:
+        video_link = reverse('demoday:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
