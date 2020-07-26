69b8adfb103a5b183eb4371704a467668569f040
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-26 18:49:18 -0300

    Criado modelos nos testes. Executado testes que necessitem de pré condição que precisam dados salvos no banco de dados e testar os casos de erro de acesso nas páginas.
    close #157

--- pypro/demoday/views.py
+++ pypro/demoday/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from pypro.demoday.models import Video
 
@@ -15,5 +15,5 @@ def indice(resquest):
 
 
 def video(resquest, slug):
-    video = Video.objects.get(slug=slug)
+    video = get_object_or_404(Video, slug=slug)
     return render(resquest, 'demoday/video.html', context={'video': video})
