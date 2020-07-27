2f4cc3a31bc78f0a1adacfa23756dc12be8534a0
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-07-26 21:33:54 -0300

    Recriado o ambiente virtual do pipenv por causa do erro de não encontrar o template do /admin
    Criado o admin.py na app demoday para poder adicionar os vídeos através do admin do Django diretamente no banco de dados.
    
    close #162

--- pypro/demoday/admin.py
+++ pypro/demoday/admin.py
@@ -6,3 +6,5 @@ from pypro.demoday.models import Video
 @register(Video)
 class VideoAdmin(ModelAdmin):
     list_display = ('titulo', 'slug', 'creation', 'vimeo_id')
+    ordering = ('creation',)
+    prepopulated_fields = {'slug': ('titulo',)}
