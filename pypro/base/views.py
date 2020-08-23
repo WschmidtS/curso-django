97968c954d8b9a2b97aad7860596a669620046d2
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-08-23 19:36:00 -0300

    Criado um processador de contexto para a aba Módulos Dessa forma não precisamos configurar em todas as views.py de cada aplicação. Fica configurado de forma global, assim a aba vai aparecer em todas as páginas do site.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,8 +1,9 @@
-
 # Create your views here.
 from django.shortcuts import render
+
+from pypro.modulos import facade
 
 
 def home(request):
     #   raise ValueError()
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {})
