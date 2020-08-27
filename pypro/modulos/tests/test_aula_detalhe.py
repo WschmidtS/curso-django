8b48d6aa99ead978ac151de0bac9052c59043011
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-08-26 21:31:12 -0300

    Implementado a funcionalidade do Breadcrumb
    close #227

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -28,3 +28,11 @@ def test_titulo(resp, aula: Aula):
 
 def test_vimeo(resp, aula: Aula):
     assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
+
+
+def test_modulo_titulo_breadcrumb(resp, modulo: Modulo):
+    assert_contains(resp, f'{modulo.titulo}')
+
+
+def test_modulo_url_breadcrumb(resp, modulo: Modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">')
