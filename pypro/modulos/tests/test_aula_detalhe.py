3eb5157c986e900f489befd5b170a9530926d33a
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-09-06 20:50:05 -0300

    Utilizado o decorator "login_required" para limitar o acesso às páginas somente para usuário logodos. Corrigido os testes das aulas que falharam porque precisam testar com usuários logados agora.
    close #270

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -17,10 +17,9 @@ def aula(modulo):
 
 
 @pytest.fixture
-def resp(client, aula):
-    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+def resp(client_com_usuario_logado, aula):
+    resp = client_com_usuario_logado.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
     return resp
-
 
 def test_titulo(resp, aula: Aula):
     assert_contains(resp, aula.titulo)
@@ -36,3 +35,14 @@ def test_modulo_titulo_breadcrumb(resp, modulo: Modulo):
 
 def test_modulo_url_breadcrumb(resp, modulo: Modulo):
     assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">')
+
+
+@pytest.fixture
+def resp_sem_usuario(client, aula):
+    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return resp
+
+
+def test_usuario_nao_logado_redirect(resp_sem_usuario):
+    assert resp_sem_usuario.status_code == 302
+    assert resp_sem_usuario.url.startswith(reverse('login'))
